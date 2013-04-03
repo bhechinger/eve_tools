a8ac21850376b7b062718cb1ce1f7041f520ebf9
Brian Hechinger <wonko@4amlunch.net>
2013-04-03 13:03:20 -0400

    now parse xml fits. Breaks with more than one, however. Need to work on that. Also switched from xmltodict to lxml.

--- EFP/GFP.py
+++ EFP/GFP.py
@@ -1,8 +1,10 @@
-import urllib, xmltodict, re
+import urllib, re, logging
 from models import Invtypes, Invnames
+from lxml import etree
 
 url="http://api.eve-central.com/api/marketstat"
 staticDB = 'eve_static'
+logger = logging.getLogger(__name__)
 
 invNames = Invnames.objects.using(staticDB)
 invTypes = Invtypes.objects.using(staticDB)
@@ -18,16 +20,39 @@ def set_item_quantity(itemData, itemName, quantity):
 		itemData.itemList[itemName] = inc
 
 def add_commas(foo):
-	return("{0:,}".format(foo))
+	return "{0:,}".format(foo)
 
-def parse_fit(fit_data):
+def parse_xml_fit(fit_data):
+	class itemData:
+		itemList = dict()
+
+	try:
+		root = etree.fromstring(fit_data)
+	except(etree.ParseError):
+		# parse error, figure out how to pass that back
+		return None
+
+	for ship in root:
+		for fitting in ship:
+			if fitting.tag != "description":
+				if fitting.tag == "shipType":
+					set_item_quantity(itemData, fitting.attrib['value'], None)
+				elif fitting.tag == "hardware":
+					if fitting.attrib['slot'] != "cargo":
+						qty = None
+						if fitting.attrib['slot'] == "drone bay":
+							qty = int(fitting.attrib['qty'])
+						set_item_quantity(itemData, fitting.attrib['type'], qty)
+
+	return itemData.itemList
+
+def parse_eft_fit(fit_data):
 	class itemData:
 		itemList = dict()
 
 	for line in fit_data.splitlines():
 		try:
 			if line[0] == "[":
-				re.IGNORECASE
 				if not re.search("^\[empty", line):
 					set_item_quantity(itemData, line.rstrip().split(",")[0][1:], None)
 			else:
@@ -41,8 +66,13 @@ def parse_fit(fit_data):
 		except:
 			pass
 
-	#return g_itemList
 	return itemData.itemList
+
+def parse_fit(fit_data):
+	if re.search("^\<\?xml", fit_data):
+		return parse_xml_fit(fit_data)
+	else:
+		return parse_eft_fit(fit_data)
 
 def fetch_itemid(itemList):
 	itemDict = dict()
@@ -66,8 +96,16 @@ def get_fit_price(form_data):
 
 	# It's easier to construct our own POST data than to use urlencode
 	post_data="usesystem={0}&typeid={1}".format(str(systemID), "&typeid=".join(itemDict))
-	f = urllib.urlopen(url, post_data)
-	doc = xmltodict.parse(f.read())
+	url_data = "{0}/?{1}".format(url, post_data)
+	logger.debug("data url: {0}".format(url_data))
+	#try:
+	#	tree = etree.parse(url_data)
+	#except(etree.XMLSyntaxError):
+	#	logger.error("XML misformed or something?")
+	#	return None
+
+	f = urllib.urlopen(url_data)
+	tree = etree.fromstring(f.read())
 	f.close()
 
 	output = dict()
@@ -75,21 +113,23 @@ def get_fit_price(form_data):
 	output['badItemList'] = list()
 	buy_total = 0
 	sell_total = 0
-	for x in doc['evec_api']['marketstat']['type']:
-		itemName = itemDict[x['@id']]
-		itemQuantity = itemList[itemName]
-		buy = int(x['buy']['max'].split(".")[0])
-		sell = int(x['sell']['min'].split(".")[0])
-		buy_subtotal = buy * itemQuantity
-		sell_subtotal = sell * itemQuantity
-		output['data'].append({'name': itemName, 'quantity': itemQuantity, 'buy': add_commas(buy), 'sell': add_commas(sell)})
-		buy_total += buy_subtotal
-		sell_total += sell_subtotal
 
+	for element in tree.iter():
+		if element.tag == "type":
+			itemName = itemDict[element.attrib['id']]
+			itemQuantity = itemList[itemName]
+			buy = float(element.xpath("buy/max")[0].text)
+			sell = float(element.xpath("sell/min")[0].text)
+			buy_subtotal = buy * itemQuantity
+			sell_subtotal = sell * itemQuantity
+			buy_total += buy_subtotal
+			sell_total += sell_subtotal
+			output['data'].append({'name': itemName, 'quantity': itemQuantity, 'buy': add_commas(buy), 'sell': add_commas(sell)})
+			
 	output['data'].append({'name': None, 'quantity': None, 'buy': add_commas(buy_total), 'sell': add_commas(sell_total)})
 
 	if badItemList:
 		for i in badItemList:
 			output['badItemList'].append(i)
 
-	return(output)
+	return output
--- eve_tools/settings.py
+++ eve_tools/settings.py
@@ -143,6 +143,7 @@ INSTALLED_APPS = (
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+SITE_ROOT = "/tmp"
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -151,18 +152,36 @@ LOGGING = {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'logfile': {
+                'level':'DEBUG',
+                'class':'logging.handlers.RotatingFileHandler',
+                'filename': SITE_ROOT + "/logfile",
+                'maxBytes': 50000,
+                'backupCount': 2,
+                'formatter': 'standard',
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'EFP': {
+                'handlers': ['logfile'],
+                'level': 'DEBUG',
         },
     }
 }
