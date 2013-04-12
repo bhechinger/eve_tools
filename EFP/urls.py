757c47534c8b0a0d76f33f4062080f7f83b466ab
Brian Hechinger <wonko@4amlunch.net>
2013-04-12 15:52:51 -0400

    oh for fuck sake

--- EFP/urls.py
+++ EFP/urls.py
@@ -6,7 +6,7 @@ from EFP import views
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<ship_id>\d+)/(?P<systemID>.+)/[Tt][Ee][Xx][Tt]/$', 'EFP.views.text', name='text'),
-	url(r'^(?P<ship_id>\d+)/(?P<systemID>.+)/[Hh[Tt][Mm][Ll]/$', 'EFP.views.html', name='html'),
+	url(r'^(?P<ship_id>\d+)/(?P<systemID>.+)/[Hh][Tt][Mm][Ll]/$', 'EFP.views.html', name='html'),
 	url(r'^(?P<ship_id>\d+)/(?P<systemID>.+)/[Xx][Mm][Ll]/$', 'EFP.views.xml', name='xml'),
 	url(r'^(?P<ship_id>\d+)/(?P<systemID>.+)/[Jj][Ss][Oo][Nn]/$', 'EFP.views.json', name='json'),
 )
