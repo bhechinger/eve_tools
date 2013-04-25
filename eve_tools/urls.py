99527223d458927e84d76806e44194ad26c542a1
Brian Hechinger <wonko@4amlunch.net>
2013-04-24 20:37:16 -0400

    cleaned up the polls app

--- eve_tools/urls.py
+++ eve_tools/urls.py
@@ -14,6 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-		url(r'^polls/', include('polls.urls', namespace="polls")),
 		url(r'^EFP/', include('EFP.urls', namespace="EFP")),
 )
