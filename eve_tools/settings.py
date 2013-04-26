a0f5c39d32d8f4e7c3bde238b830439eb5cce057
Brian Hechinger <wonko@4amlunch.net>
2013-04-26 16:21:07 -0400

    changed dev to use different db

--- eve_tools/settings.py
+++ eve_tools/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'eve_tools',                      # Or path to database file if using sqlite3.
+        'NAME': 'eve_tools_dev',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'etadmin',
         'PASSWORD': '#%Qej8h',
